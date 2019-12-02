ddfb6798f8b72ce8bcb44a077646058c06448edf
FrankBohan <frank@shipt.com>
2019-12-02 17:06:14 -0600

    Play with position for frames 2-12

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	const X = 300
 	const Y = 149
-	im, err := gg.LoadImage("resources/frames/frame_00_delay-0.06s.jpg")
+	im, err := gg.LoadImage("resources/frames/frame_02_delay-0.06s.jpg")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -17,13 +17,13 @@ func main() {
 	dc.SetRGB(1, 1, 1)
 	dc.Clear()
 	dc.SetRGB(1, 1, 1)
-	if err := dc.LoadFontFace("resources/Cocktail.ttf", 16); err != nil {
+	if err := dc.LoadFontFace("resources/Cocktail.ttf", 18); err != nil {
 		panic(err)
 	}
 
 	dc.DrawRoundedRectangle(0, 0, X, Y, 0)
 	dc.DrawImage(im, 0, 0)
-	dc.DrawStringAnchored("FRANK", X/2, Y/2, 0.5, 0.5)
+	dc.DrawStringAnchored("FRANK", 197, 132, 0.5, 0.5)
 	dc.Clip()
 	dc.SavePNG("out.png")
 }
