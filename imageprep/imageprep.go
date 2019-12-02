58f6c115c3fe3568a0f80c342d3cb345a1871acb
FrankBohan <frank@shipt.com>
2019-12-02 17:53:57 -0600

    Freaking slices

--- imageprep/imageprep.go
+++ imageprep/imageprep.go
@@ -1,13 +1,31 @@
 package imageprep
 
 import (
+	"fmt"
 	"github.com/fogleman/gg"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 func ImagePrep(name string) {
 	const X = 300
 	const Y = 149
+	var frames []string
+
+	// Get array of paths for frames.
+	err := filepath.Walk("resources/frames", func(path string, info os.FileInfo, err error) error {
+		frames = append(frames, path)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(frames); i++ {
+		fmt.Printf("%02d\n", i)
+	}
+
 	im, err := gg.LoadImage("resources/frames/frame_02_delay-0.06s.jpg")
 	if err != nil {
 		log.Fatal(err)
