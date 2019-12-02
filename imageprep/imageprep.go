ae491392974a16c2b65e444d4ae4c7ccf10a1671
FrankBohan <frank@shipt.com>
2019-12-02 17:23:34 -0600

    Move logic to prep package

---
+++ imageprep/imageprep.go
@@ -0,0 +1,29 @@
+package imageprep
+
+import (
+	"github.com/fogleman/gg"
+	"log"
+)
+
+func ImagePrep(name string) {
+	const X = 300
+	const Y = 149
+	im, err := gg.LoadImage("resources/frames/frame_02_delay-0.06s.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dc := gg.NewContext(X, Y)
+	dc.SetRGB(1, 1, 1)
+	dc.Clear()
+	dc.SetRGB(1, 1, 1)
+	if err := dc.LoadFontFace("resources/Cocktail.ttf", 18); err != nil {
+		panic(err)
+	}
+
+	dc.DrawRoundedRectangle(0, 0, X, Y, 0)
+	dc.DrawImage(im, 0, 0)
+	dc.DrawStringAnchored(name, 197, 132, 0.5, 0.5)
+	dc.Clip()
+	dc.SavePNG("out.jpg")
+}
--- main.go
+++ main.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"github.com/fogleman/gg"
-	"log"
-)
+import img "github.com/francisbohan/GoEatShit/imageprep"
 
 func main() {
-	const X = 300
-	const Y = 149
-	im, err := gg.LoadImage("resources/frames/frame_02_delay-0.06s.jpg")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	dc := gg.NewContext(X, Y)
-	dc.SetRGB(1, 1, 1)
-	dc.Clear()
-	dc.SetRGB(1, 1, 1)
-	if err := dc.LoadFontFace("resources/Cocktail.ttf", 18); err != nil {
-		panic(err)
-	}
-
-	dc.DrawRoundedRectangle(0, 0, X, Y, 0)
-	dc.DrawImage(im, 0, 0)
-	dc.DrawStringAnchored("FRANK", 197, 132, 0.5, 0.5)
-	dc.Clip()
-	dc.SavePNG("out.png")
+	img.ImagePrep("FRANK")
 }
