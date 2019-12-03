f65eab33697de7d8c5e2712d9e42a672c8de803f
FrankBohan <frank@shipt.com>
2019-12-02 18:38:56 -0600

    Meh

--- imageprep/imageprep.go
+++ imageprep/imageprep.go
@@ -18,34 +18,36 @@ func ImagePrep(name string) {
 
 	// Get array of paths for frames.
 	err := filepath.Walk("resources/frames", func(path string, info os.FileInfo, err error) error {
-		frames = append(frames, path)
+		frames = append(frames, path)[1:]
 		return nil
 	})
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(frames)
-	for i := 0; i < len(frames); i++ {
-		fmt.Printf("%02d\n", i)
-	}
+	// fmt.Printf("%v", frames)
+	for frame := range frames {
+		frame := strings.Split(frames[frame], "/")
+		iframe := frame[len(frame)-1]
+		fmt.Printf("frame: %s\n", iframe)
 
-	im, err := gg.LoadImage("resources/frames/frame_02_delay-0.06s.jpg")
-	if err != nil {
-		log.Fatal(err)
-	}
+		im, err := gg.LoadImage(fmt.Sprintf("resources/frames/%s", iframe))
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	dc := gg.NewContext(X, Y)
-	dc.SetRGB(1, 1, 1)
-	dc.Clear()
-	dc.SetRGB(1, 1, 1)
-	if err := dc.LoadFontFace("resources/Cocktail.ttf", 20); err != nil {
-		panic(err)
-	}
+		dc := gg.NewContext(X, Y)
+		dc.SetRGB(1, 1, 1)
+		dc.Clear()
+		dc.SetRGB(1, 1, 1)
+		if err := dc.LoadFontFace("resources/Cocktail.ttf", 20); err != nil {
+			panic(err)
+		}
 
-	dc.DrawRoundedRectangle(0, 0, X, Y, 0)
-	dc.DrawImage(im, 0, 0)
-	dc.DrawStringAnchored(strings.ToUpper(name), 199, 133, 0.5, 0.5)
-	dc.Clip()
-	dc.SavePNG("outputframes/out.jpg")
+		dc.DrawRoundedRectangle(0, 0, X, Y, 0)
+		dc.DrawImage(im, 0, 0)
+		dc.DrawStringAnchored(strings.ToUpper(name), 199, 133, 0.5, 0.5)
+		dc.Clip()
+		dc.SavePNG(fmt.Sprintf("resources/outputframes/%s", iframe))
+	}
 }
